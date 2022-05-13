68192ad2f9e05bb9a6b7272afe73757cd6a9fcfd
Esther Murira <muriraesther47@gmail.com>
2022-05-13 12:50:00 +0300

    first commit

---
+++ 1.EngineTrendMonitoringTechlogSp.sql
@@ -0,0 +1,30 @@
+USE [ShearAccAviaV6.0]
+GO
+
+/****** Object:  StoredProcedure [dbo].[EngineTrendMonitoringTechlogSp]    Script Date: 5/12/2022 10:06:20 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		Gitata M.M.
+-- Create date: 16/10/12
+-- Description:	get the techlog for a specific TechLogNo
+-- =============================================
+CREATE PROCEDURE [dbo].[EngineTrendMonitoringTechlogSp]
+
+	@TechLogNo INT
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT * FROM EngineTrendMonitoring WHERE TechLogNo = @TechLogNo
+END
+GO
+
